4d2f3ef9e132157b292e3f1a12657b0da91e7100
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-03 23:20:24 -0400

    Use url.Parse to allow setting target including scheme
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.go
+++ main.go
@@ -9,15 +9,12 @@ import (
 )
 
 type makeGray struct {
-	domain string
+	host url.URL
 }
 
 func (mk makeGray) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	target := url.URL{
-		Scheme: "https",
-		Host:   mk.domain,
-		Path:   request.URL.Path,
-	}
+	target := mk.host
+	target.Path = request.URL.Path
 
 	fetch := http.Request{
 		Method: "GET",
@@ -43,9 +40,14 @@ func (mk makeGray) ServeHTTP(writer http.ResponseWriter, request *http.Request) 
 func main() {
 	const addr = "127.0.0.1:8080"
 
-	domain := flag.String("domain", "maps.wikimedia.org", "The origin server against which incoming requests will be proxied")
+	host := flag.String("host", "https://maps.wikimedia.org", "The origin server against which incoming requests will be proxied")
 	flag.Parse()
 
-	log.Printf("Starting server on %s, proxying %s", addr, *domain)
-	http.ListenAndServe(addr, &makeGray{*domain})
+	url, err := url.Parse(*host)
+	if err != nil {
+		log.Printf("Invalid target host, %s. Please provide a valid URL", *host)
+	}
+
+	log.Printf("Starting server on %s, proxying %s", addr, url)
+	http.ListenAndServe(addr, &makeGray{*url})
 }
