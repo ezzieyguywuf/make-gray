c5024173e4d7ca8440c7fc829872cf4d14e51d82
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-03 22:08:52 -0400

    Use log instead of fmt for logging
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 type makeGray struct{}
 
 func (makeGray) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println(request.RemoteAddr, " ", request.Method, " ", request.URL)
+	log.Println(request.RemoteAddr, " ", request.Method, " ", request.URL)
 }
 
 func main() {
-	fmt.Println("Starting server on 127.0.0.1:8080")
+	log.Println("Starting server on 127.0.0.1:8080")
 
 	http.ListenAndServe("127.0.0.1:8080", &makeGray{})
 }
