2bb9d75a42feffb90e5de4c7913a8005e3757dea
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-03 22:22:25 -0400

    Add flag to specify target origin server
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-type makeGray struct{}
+type makeGray struct {
+	domain string
+}
 
 func (makeGray) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("%s %s %s", request.RemoteAddr, request.Method, request.URL)
@@ -13,7 +16,10 @@ func (makeGray) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	const addr = "127.0.0.1:8080"
-	log.Printf("Starting server on %s", addr)
 
-	http.ListenAndServe(addr, &makeGray{})
+	domain := flag.String("domain", "https://maps.wikimedia.org", "The origin server against which incoming requests will be proxied")
+	flag.Parse()
+
+	log.Printf("Starting server on %s, proxying %s", addr, *domain)
+	http.ListenAndServe(addr, &makeGray{*domain})
 }
