1200d0643d108b04a34506d1beebe98995ae85e0
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-03 22:03:13 -0400

    Add basic http server
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type makeGray struct{}
+
+func (makeGray) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println(request.RemoteAddr, " ", request.Method, " ", request.URL)
+}
 
 func main() {
-	fmt.Println("Hello, make-gray!")
+	fmt.Println("Starting server on 127.0.0.1:8080")
+
+	http.ListenAndServe("127.0.0.1:8080", &makeGray{})
 }
